4bf89933a88e84fc76f415ec2c2311503999ad50
Pavel Butuzov <pavel.butuzov@testit.software>
2022-07-25 16:53:08 +0300

    Fixed of order of setup/teardown.

--- testit-importer-allure/setup.py
+++ testit-importer-allure/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='testit-importer-allure',
-    version='1.0.0',
+    version='1.1.1',
     description='Allure report importer for Test IT',
     long_description=open('README.md', "r").read(),
     long_description_content_type="text/markdown",
@@ -20,6 +20,6 @@ setup(
     packages=['testit_importer_allure'],
     package_data={'testit_importer_allure': ['../connection_config.ini']},
     package_dir={'testit_importer_allure': 'src'},
-    install_requires=['testit-api-client'],
+    install_requires=['testit-api-client>=1.1,<2', 'xmltodict'],
     entry_points={'console_scripts': ['testit = testit_importer_allure.__main__:console_main']}
 )
--- testit-importer-allure/src/utils.py
+++ testit-importer-allure/src/utils.py
@@ -9,31 +9,32 @@ def form_steps(steps, requests, path):
         steps, prefix = mapping_xml_values(steps, 'step', 'status')
 
         for step in steps:
-            if 'steps' in step:
-                inner_steps, inner_results_steps = form_steps(step['steps'], requests, path)
-            else:
-                inner_steps = []
-                inner_results_steps = []
+            if 'name' in step:
+                if 'steps' in step:
+                    inner_steps, inner_results_steps = form_steps(step['steps'], requests, path)
+                else:
+                    inner_steps = []
+                    inner_results_steps = []
 
-            adapt_steps.append(
-                {
-                    'title': step['name'],
-                    'steps': inner_steps
-                }
-            )
+                adapt_steps.append(
+                    {
+                        'title': step['name'],
+                        'steps': inner_steps
+                    }
+                )
 
-            attachments = get_attachment(requests, step['attachments'], path) if 'attachments' in step else []
+                attachments = get_attachment(requests, step['attachments'], path) if 'attachments' in step else []
 
-            results_steps.append(
-                {
-                    'title': step['name'],
-                    'stepResults': inner_results_steps,
-                    'outcome': step[f'{prefix}status'].title() if step[f'{prefix}status'] in ('passed', 'skipped') else 'Failed',
-                    'duration': (int(step[f'{prefix}stop']) - int(step[f'{prefix}start'])) if f'{prefix}stop' in step else 0,
-                    "attachments": attachments,
-                    'parameters': form_parameters(step['parameters']) if 'parameters' in step else None
-                }
-            )
+                results_steps.append(
+                    {
+                        'title': step['name'],
+                        'stepResults': inner_results_steps,
+                        'outcome': step[f'{prefix}status'].title() if step[f'{prefix}status'] in ('passed', 'skipped') else 'Failed',
+                        'duration': (int(step[f'{prefix}stop']) - int(step[f'{prefix}start'])) if f'{prefix}stop' in step else 0,
+                        "attachments": attachments,
+                        'parameters': form_parameters(step['parameters']) if 'parameters' in step else None
+                    }
+                )
 
     return adapt_steps, results_steps
 
@@ -44,20 +45,23 @@ def form_labels_namespace_classname_workitems_id(allure_labels):
     labels = []
     workitems_id = []
 
-    allure_labels, prefix = mapping_xml_values(allure_labels, 'label', 'value')
+    if allure_labels:
+        allure_labels, prefix = mapping_xml_values(allure_labels, 'label', 'value')
 
-    for label in allure_labels:
-        if label[f'{prefix}name'] == 'testcase':
-            workitems_id.append(label[f'{prefix}value'])
-        else:
-            labels.append({'name': f"{label[f'{prefix}name']}:{label[f'{prefix}value']}"})
+        for label in allure_labels:
+            if label[f'{prefix}name'] == 'testcase':
+                workitems_id.append(label[f'{prefix}value'])
+            else:
+                labels.append({'name': f"{label[f'{prefix}name']}:{label[f'{prefix}value']}"})
 
-        if label[f'{prefix}name'] == 'package':
-            namespace = label[f'{prefix}value'].split('.')[-1]
-        elif label[f'{prefix}name'] == 'subSuite':
-            classname = label[f'{prefix}value']
-        elif label[f'{prefix}name'] == 'testClass':
-            classname = label[f'{prefix}value'].split('.')[-1]
+            if label[f'{prefix}name'] == 'package':
+                namespace = label[f'{prefix}value'].split('.')[-1]
+            elif label[f'{prefix}name'] == 'parentSuite':
+                namespace = label[f'{prefix}value']
+            elif label[f'{prefix}name'] in ('subSuite', 'suite'):
+                classname = label[f'{prefix}value']
+            elif label[f'{prefix}name'] == 'testClass':
+                classname = label[f'{prefix}value'].split('.')[-1]
 
     return labels, namespace, classname, workitems_id
 
@@ -65,39 +69,41 @@ def form_labels_namespace_classname_workitems_id(allure_labels):
 def form_links(allure_links):
     links = []
 
-    for link in allure_links:
-        links.append({})
+    if allure_links:
+        for link in allure_links:
+            links.append({})
 
-        if 'url' in link and re.fullmatch(r'^(?:(?:(?:https?|ftp):)?\/\/)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-zA-Z0-9\u00a1-\uffff][a-zA-Z0-9\u00a1-\uffff_-]{0,62})?[a-zA-Z0-9\u00a1-\uffff]\.)+(?:[a-zA-Z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$', link['url']):
-            links[-1]['url'] = link['url']
-        else:
-            raise Exception('Some links have the wrong URL or no URL!')
+            if 'url' in link and re.fullmatch(r'^(?:(?:(?:https?|ftp):)?\/\/)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-zA-Z0-9\u00a1-\uffff][a-zA-Z0-9\u00a1-\uffff_-]{0,62})?[a-zA-Z0-9\u00a1-\uffff]\.)+(?:[a-zA-Z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$', link['url']):
+                links[-1]['url'] = link['url']
+            else:
+                raise Exception('Some links have the wrong URL or no URL!')
 
-        if 'type' in link and link['type'] in ('Related', 'BlockedBy', 'Defect', 'Issue', 'Requirement', 'Repository'):
-            links[-1]['type'] = link['type']
+            if 'type' in link and link['type'] in ('Related', 'BlockedBy', 'Defect', 'Issue', 'Requirement', 'Repository'):
+                links[-1]['type'] = link['type']
 
-        if 'name' in link:
-            links[-1]['title'] = link['name']
+            if 'name' in link:
+                links[-1]['title'] = link['name']
 
     return links
 
 
-def form_setup_teardown(data_before_after, uuid, requests, path):
+def form_setup_teardown(data_before_after, test_uuid, requests, path):
     setup = []
     teardown = []
     results_setup = []
     results_teardown = []
 
-    if uuid:
-        for time in data_before_after:
-            for child in data_before_after[time]['children']:
-                if child == uuid:
-                    steps, results_steps = form_steps(data_before_after[time]['befores'], requests, path)
-                    setup += steps
-                    results_setup += results_steps
-                    steps, results_steps = form_steps(data_before_after[time]['afters'], requests, path)
-                    teardown += steps
-                    results_teardown += results_steps
+    if test_uuid:
+        for uuid in data_before_after:
+            if 'children' in data_before_after[uuid]:
+                for child in data_before_after[uuid]['children']:
+                    if child == test_uuid:
+                        steps, results_steps = form_steps(data_before_after[uuid].get('befores'), requests, path)
+                        setup += steps
+                        results_setup += results_steps
+                        steps, results_steps = form_steps(data_before_after[uuid].get('afters'), requests, path)
+                        teardown += steps
+                        results_teardown += results_steps
 
     return setup, results_setup, teardown, results_teardown
 
@@ -105,10 +111,11 @@ def form_setup_teardown(data_before_after, uuid, requests, path):
 def form_parameters(allure_parameters):
     parameters = {}
 
-    allure_parameters, prefix = mapping_xml_values(allure_parameters, 'parameter', 'value')
+    if allure_parameters:
+        allure_parameters, prefix = mapping_xml_values(allure_parameters, 'parameter', 'value')
 
-    for parameter in allure_parameters:
-        parameters[parameter[f'{prefix}name']] = parameter[f'{prefix}value']
+        for parameter in allure_parameters:
+            parameters[parameter[f'{prefix}name']] = parameter[f'{prefix}value']
 
     return parameters
 
@@ -126,7 +133,10 @@ def get_attachment(requests, attachments_sources, path):
         prefix = '' if 'source' in attachments_sources[0] else '@'
 
         for attachment_source in attachments_sources:
-            attachments.append({'id': requests.load_attachment(open(f"{path}/{attachment_source[f'{prefix}source']}", 'rb'))})
+            attachment_id = requests.load_attachment(open(f"{path}/{attachment_source[f'{prefix}source']}", 'rb'))
+
+            if attachment_id:
+                attachments.append({'id': attachment_id})
 
     return attachments
 
