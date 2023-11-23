828fb793ad74958264af39b4072331462fcef839
Pavel Butuzov <73579433+PavelButuzov@users.noreply.github.com>
2023-11-23 13:30:58 +0300

    Fixed parsing of a step without a status. (#49)
    * Fixed parsing of a step without a status.
    
    * Fixed parsing of a link type.
    
    ---------
    
    Co-authored-by: pavel.butuzov <pavel.butuzov@testit.software>

--- testit-importer-allure/setup.py
+++ testit-importer-allure/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='testit-importer-allure',
-    version='1.4.0',
+    version='1.4.1',
     description='Allure report importer for Test IT',
     long_description=open('README.md', "r").read(),
     long_description_content_type="text/markdown",
