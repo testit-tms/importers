d0e2a2d2021d9cb4e820425fc61dc848589305a8
Pavel Butuzov <73579433+PavelButuzov@users.noreply.github.com>
2025-05-21 09:49:29 +0300

    fix: TMS-32730: delete 'python-interface' package from requires for python 3.13. (#77)
    Co-authored-by: pavel.butuzov <pavel.butuzov@testit.software>

--- testit-importer-allure/src/reader.py
+++ testit-importer-allure/src/reader.py
@@ -1,16 +1,21 @@
-from interface import Interface
+from abc import ABCMeta, abstractmethod
 
 
-class Reader(Interface):
+class Reader:
+    __metaclass__ = ABCMeta
 
+    @abstractmethod
     def get_all_files(self):
         pass
 
+    @abstractmethod
     def read_file(self, file_name: str):
         pass
 
+    @abstractmethod
     def read_attachment(self, file_name: str):
         pass
 
+    @abstractmethod
     def remove_attachment(self, file_name: str):
         pass
