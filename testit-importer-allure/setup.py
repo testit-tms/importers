afaf8220bfd8ebb24be6096f71642b7a78227fcd
Pavel Butuzov <73579433+PavelButuzov@users.noreply.github.com>
2025-09-11 22:21:17 +0300

    feat: support the api-client 7.1. (#87)
    Co-authored-by: pavel.butuzov <pavel.butuzov@testit.software>

--- testit-importer-allure/setup.py
+++ testit-importer-allure/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = "1.13.0"
+VERSION = "1.14.0"
 
 setup(
     name='testit-importer-allure',
@@ -25,7 +25,7 @@ setup(
     package_data={'testit_importer_allure': ['../connection_config.ini']},
     package_dir={'testit_importer_allure': 'src'},
     install_requires=[
-        'testit-api-client==7.0.1',
+        'testit-api-client==7.1.0',
         'xmltodict',
         'minio',
         'pika'
