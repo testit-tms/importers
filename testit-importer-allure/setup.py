58553b85f486db2b88b0998c9736de42c579298a
Dmitry Ermakovich <dmitry.ermakovich@testit.software>
2026-08-28 14:30:38 +0300

    feat: cloud support

--- testit-importer-allure/setup.py
+++ testit-importer-allure/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = "1.17.4"
+VERSION = "1.17.5"
 
 setup(
     name='testit-importer-allure',
@@ -25,7 +25,7 @@ setup(
     package_data={'testit_importer_allure': ['../connection_config.ini']},
     package_dir={'testit_importer_allure': 'src'},
     install_requires=[
-        'testit-api-client==7.5.12',
+        'testit-api-client==7.6.1',
         'xmltodict',
         'minio',
         'pika'
